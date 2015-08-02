947e216c42f3add2720f1c933fa7224d7fb62633
Ritwik Gupta <ritwikg2004@live.com>
2015-08-01 23:09:13 -0700

    Finished web wrapper

--- apiwrapper/views.py
+++ apiwrapper/views.py
@@ -6,25 +6,44 @@ course = CourseAPI()
 laundry = LaundryAPI()
 labs = LabAPI()
 
-@apiwrapper.route('/get_courses/<term>/<subject>', strict_slashes=False)
+@apiwrapper.route('/courses/<term>/<subject>', strict_slashes=False)
 def get_courses(term='-1', subject='-1'):
     if term == '-1' or subject == '-1':
         raise InvalidParameterException()
 
     return Response(str(course.get_courses(term, subject)), mimetype='application/json')
 
-@apiwrapper.route('/get_courses_by_req/<term>/<req>', strict_slashes=False)
+@apiwrapper.route('/courses_by_req/<term>/<req>', strict_slashes=False)
 def get_courses_by_req(term='-1', req='-1'):
     if term == '-1' or req == '-1':
         raise InvalidParameterException()
-    
+
     return Response(str(course.get_courses_by_req(term, req)), mimetype='application/json')
 
-@apiwrapper.route('/get_class_description/<class_number>/<term>', strict_slashes=False)
-def get_class_description(class_number="-1", term="-1"):
+@apiwrapper.route('/class_description/<class_number>/<term>', strict_slashes=False)
+def get_class_description(class_number='-1', term='-1'):
     if term == '-1' or class_number == '-1':
             raise InvalidParameterException()
 
-    return Response(str(course.get_class_description(class_number, term)), mimetype='application/json')
+    return Response(str(course.get_class_description(class_number, term)), mimetype='text/plain')
 
+@apiwrapper.route('/lab_status/<lab_name>', strict_slashes=False)
+def get_lab_status(lab_name='-1'):
+    if lab_name == '-1':
+        raise InvalidParameterException()
 
+    return Response(str(labs.get_status(lab_name)), mimetype='application/json')
+
+@apiwrapper.route('/laundry_simple/<loc>', strict_slashes=False)
+def get_laundry_status_simple(loc='-1'):
+    if loc == '-1':
+        raise InvalidParameterException()
+
+    return Response(str(laundry.get_status_simple(loc)), mimetype='application/json')
+
+@apiwrapper.route('/laundry_detailed/<loc>', strict_slashes=False)
+def get_laundry_status_detailed(loc='-1'):
+    if loc == '-1':
+        raise InvalidParameterException()
+
+    return Response(str(laundry.get_status_detailed(loc)), mimetype='application/json')
