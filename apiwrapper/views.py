56cbaba22846b5356986798de4b9ff4e78492e35
vonbearshark <jjnaughton93@gmail.com>
2017-01-30 13:17:34 -0500

    Rather than convert to string and Response object, use jsonify, which returns a Response object we can send directly

--- apiwrapper/views.py
+++ apiwrapper/views.py
@@ -21,42 +21,42 @@ def get_courses(term='-1', subject='-1'):
     if term == '-1' or subject == '-1':
         raise InvalidParameterException()
 
-    return Response(str(course.get_courses(term, subject)), mimetype='application/json')
+    return jsonify(course.get_courses(term, subject))
 
 @apiwrapper.route('/courses_by_req/<term>/<req>', strict_slashes=False)
 def get_courses_by_req(term='-1', req='-1'):
     if term == '-1' or req == '-1':
         raise InvalidParameterException()
 
-    return Response(str(course.get_courses_by_req(term, req)), mimetype='application/json')
+    return jsonify(course.get_courses_by_req(term, req))
 
 @apiwrapper.route('/class_description/<class_number>/<term>', strict_slashes=False)
 def get_class_description(class_number='-1', term='-1'):
     if term == '-1' or class_number == '-1':
             raise InvalidParameterException()
 
-    return Response(str(course.get_class_description(class_number, term)), mimetype='text/plain')
+    return jsonify(course.get_class_description(class_number, term))
 
 @apiwrapper.route('/lab_status/<lab_name>', strict_slashes=False)
 def get_lab_status(lab_name='-1'):
     if lab_name == '-1':
         raise InvalidParameterException()
 
-    return Response(str(labs.get_status(lab_name)), mimetype='application/json')
+    return jsonify(labs.get_status(lab_name))
 
 @apiwrapper.route('/laundry_simple/<loc>', strict_slashes=False)
 def get_laundry_status_simple(loc='-1'):
     if loc == '-1':
         raise InvalidParameterException()
 
-    return Response(str(laundry.get_status_simple(loc)), mimetype='application/json')
+    return jsonify(laundry.get_status_simple(loc))
 
 @apiwrapper.route('/laundry_detailed/<loc>', strict_slashes=False)
 def get_laundry_status_detailed(loc='-1'):
     if loc == '-1':
         raise InvalidParameterException()
 
-    return Response(str(laundry.get_status_detailed(loc)), mimetype='application/json')
+    return jsonify(laundry.get_status_detailed(loc))
 
 if __name__ == '__main__':
   apiwrapper.run(debug=True, port=8000)
